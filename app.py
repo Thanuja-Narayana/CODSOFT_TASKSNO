13002dac36036f91e3668c5c0afd72e8ef363b10
Thanuja-Narayana <thanujak1310@gmail.com>
2026-08-11 19:42:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+# Simple Login Program
+
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+if username == "admin" and password == "admin123":
+    print("Login Successful")
+else:
+    print("Login Failed")
